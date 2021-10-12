5dab13c12cfd70e98a07d930ff4b332f6c1365eb
Ashish <ashishkumarvish.432@gmail.com>
2021-10-12 20:21:00 +0530

    Present or absent

--- src/com/bridgelab/EmployeeWage.java
+++ src/com/bridgelab/EmployeeWage.java
@@ -2,6 +2,16 @@ package com.bridgelab;
 
 public class EmployeeWage {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage");
+
+        // constants
+        int IS_PRESENT = 1;
+        double empCheck = Math.floor(Math.random() * 10) %2;
+        // using random class to get random number
+        if (empCheck == IS_PRESENT) { // empCheck get 1 when the first condition is true and employee is present
+            System.out.println("Employee is Present");
+        }
+        else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
