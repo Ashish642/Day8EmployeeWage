97f7c0cfc66cf719d8012a727b80e46c2d9b064d
Ashish <ashishkumarvish.432@gmail.com>
2021-10-12 20:57:54 +0530

    To add full time or part time

--- src/com/bridgelab/EmployeeWage.java
+++ src/com/bridgelab/EmployeeWage.java
@@ -4,16 +4,21 @@ public class EmployeeWage {
     public static void main(String[] args) {
 
         // constants
-        int IS_PRESENT = 1;
+        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
         int ENP_RATE_PER_HOUR = 20;
+
         // Variable
         int empHr = 0;
         int empWage = 0;
 
         // computation
-        double empCheck = Math.floor(Math.random() * 10) %2;
+        double empCheck = Math.floor(Math.random() * 10) %3;
         // using random class to get random number
-        if (empCheck == IS_PRESENT) { // empCheck get 1 when the first condition is true and employee is present
+        if (empCheck == IS_PART_TIME) { // empCheck get 1 when the first condition is true and employee is present
+            empHr = 4;
+        }
+        else if (empCheck == IS_FULL_TIME) {
             empHr = 8;
         }
         else {
