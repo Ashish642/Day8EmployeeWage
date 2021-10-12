15660fefb9e748a81af42d5024a054fa507b52cd
Ashish <ashishkumarvish.432@gmail.com>
2021-10-12 21:15:21 +0530

    From Switch case

--- src/com/bridgelab/EmployeeWage.java
+++ src/com/bridgelab/EmployeeWage.java
@@ -1,28 +1,28 @@
 package com.bridgelab;
 
 public class EmployeeWage {
-    public static void main(String[] args) {
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+   public static final int ENP_RATE_PER_HOUR = 20;
 
-        // constants
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-        int ENP_RATE_PER_HOUR = 20;
 
-        // Variable
+   public static void main(String[] args) {
+       // Variable
         int empHr = 0;
         int empWage = 0;
 
         // computation
-        double empCheck = Math.floor(Math.random() * 10) %3;
+        int empCheck = (int) Math.floor(Math.random() * 10) %3;
         // using random class to get random number
-        if (empCheck == IS_PART_TIME) { // empCheck get 1 when the first condition is true and employee is present
+        switch (empCheck) {
+            case IS_PART_TIME:
             empHr = 4;
-        }
-        else if (empCheck == IS_FULL_TIME) {
-            empHr = 8;
-        }
-        else {
-            empHr = 0;
+            break;
+            case IS_FULL_TIME:
+                empHr = 8;
+                break;
+            default:
+                empHr = 0;
 
         }
         empWage = empHr * ENP_RATE_PER_HOUR;
